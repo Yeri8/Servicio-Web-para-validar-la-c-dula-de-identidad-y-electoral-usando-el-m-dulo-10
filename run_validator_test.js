83fbd4c4a3665822e5900e1f0091c4693e48635f
Yeri8 <enmanuelg725@gmail.com>
2025-11-30 18:44:35 -0400

    Delete run_validator_test.js

--- run_validator_test.js
+++
@@ -1,6 +0,0 @@
-const { module10Validate } = require('./module10-validator');
-const tests = ['79927398713','00113777772','40212054885','40200700675','12345678901','001-1377777-2'];
-for(const t of tests){
-  const r = module10Validate(t);
-  console.log(t, '=>', r.valid ? 'CÉDULA ES CORRECTA' : 'CÉDULA ES INCORRECTA', r);
-}
