e51268d80835f67520c23c59bea232a499109fa9
Yeri8 <enmanuelg725@gmail.com>
2025-11-30 19:05:23 -0400

    Update validator.js

--- public/validator.js
+++ public/validator.js
@@ -1,19 +1,11 @@
-function validarCedula(num) {
-    num = num.replace(/-/g, "");
-    if (!/^[0-9]{11}$/.test(num)) return false;
-
-    let sum = 0;
-    let alt = false;
-
-    for (let i = num.length - 1; i >= 0; i--) {
-        let n = parseInt(num[i]);
-        if (alt) {
-            n *= 2;
-            if (n > 9) n -= 9;
-        }
-        sum += n;
-        alt = !alt;
-    }
-
-    return sum % 10 === 0;
+function validarCedula(num){
+ num=num.replace(/-/g,'');
+ if(!/^[0-9]{11}$/.test(num)) return false;
+ let sum=0,alt=false;
+ for(let i=num.length-1;i>=0;i--){
+   let n=parseInt(num[i]);
+   if(alt){n*=2;if(n>9)n-=9;}
+   sum+=n;alt=!alt;
+ }
+ return sum%10===0;
 }
